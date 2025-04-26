be50dd8c8e51be3d8661bf781bd26cfd61f30a3a
Raiden1121 <ray.working.for@gmail.com>
2025-04-27 04:20:35 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import AIDesignPage from "./pages/AIDesignPage";
 function App() {
   return (
     <>
-      <img src="https://s3.us-west-2.amazonaws.com/generate.imagenfile/uploads/b44537a5-9742-4f09-93e0-a4cd17d35317.jpg"/>
       <AIDesignPage />
       {/* <HtmlPage /> */}
     </>

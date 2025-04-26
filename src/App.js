07ac32dcbb6ffa4cf275d861b19089a7fbb03dba
jmyang0928 <106552960+jmyang0928@users.noreply.github.com>
2025-04-27 04:03:56 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import AIDesignPage from "./pages/AIDesignPage";
 // import HtmlPage from './pages/HtmlPage.jsx';
+import { StorageImage } from '@aws-amplify/ui-react-storage';
+
 
 
 function App() {
   return (
     <>
-      <img src="https://s3.us-west-2.amazonaws.com/generate.imagenfile/uploads/b44537a5-9742-4f09-93e0-a4cd17d35317.jpg"/>
+      <StorageImage
+        alt="Example Image"
+        path="https://s3.us-west-2.amazonaws.com/generate.imagenfile/uploads/b44537a5-9742-4f09-93e0-a4cd17d35317.jpg"
+      />
       <AIDesignPage />
       {/* <HtmlPage /> */}
     </>

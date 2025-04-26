b37646398ed2b165f78d838704f992cae243ddfb
Raiden1121 <ray.working.for@gmail.com>
2025-04-27 03:05:52 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React from "react";
 import AIDesignPage from "./pages/AIDesignPage";
+// import HtmlPage from './pages/HtmlPage.jsx';
+
 
 function App() {
-  return <AIDesignPage />;
+  return (
+    <>
+      <img src="https://s3.us-west-2.amazonaws.com/generate.imagenfile/uploads/b44537a5-9742-4f09-93e0-a4cd17d35317.jpg"/>
+      <AIDesignPage />
+      {/* <HtmlPage /> */}
+    </>
+  );
 }
 
 export default App;
